3ab715e9e122676d1dddb07b9ea1009d90424ff9
sd12key <166713967+sd12key@users.noreply.github.com>
2025-07-19 19:01:04 -0230

    Initial commit

---
+++ src/main/java/org/alvio/golfnode/RestServiceApplication.java
@@ -0,0 +1,12 @@
+package org.alvio.golfnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+public class RestServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+}
